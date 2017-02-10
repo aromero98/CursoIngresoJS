d7759f1936a3f4bc2f830a7b6fd626634588f132
aromero98 <agustinleandroromero@gmail.com>
2017-02-09 22:07:11 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,19 @@
 function Mostrar()
 {
-//tomo la edad  
-
-
-
-
+//tomo la edad 
+	var edad;
+  	edad=document.getElementById('edad').value; 
+  	if(edad>17)
+  	{
+  		alert("mayor");
+  	}
+  	
+  	if(edad<18 && edad>12)
+  	{
+  		alert("adolescente");
+  	}
+  	if(edad<13)
+  	{
+  		alert("menor");
+  	}
 }//FIN DE LA FUNCIÓN
