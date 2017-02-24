4d0632841ed326e79ca7d1521ca715edafa0c882
aromero98 <agustinleandroromero@gmail.com>
2017-02-23 22:03:35 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,75 @@
 function Mostrar()
 {
+ 	var cont=0;
+ 	var numerorandom;
+ 	var cont1=0;
+ 	var cont2=0;
+ 	var cont3=0;
+ 	var cont4=0;
+ 	var cont5=0;
+ 	var cont6=0;
+ 	var cont7=0;
+ 	var cont8=0;
+ 	var cont9=0;
+ 	var cont10=0;
+
+
+ 	for(;cont<100000;cont++)                        //siempre debe aparecer los dos puntos y coma
+ 	{
+ 		
+ 		cont++;
+ 		numerorandom= Math.floor((Math.random() * 10) + 1);
+ 		//console.log(numerorandom);
+		
+		switch(numerorandom)
+		{
+			case 1:
+				cont1++;
+				break;
+			case 2:
+				cont2++;
+				break
+			case 3:
+				cont3++;
+				break;
+			case 4:
+				cont4++;
+				break;
+			case 5:
+				cont5++;
+				break;
+			case 6:
+				cont6++;
+				break;
+			if(cont==7)
+			{
+				continue;
+			}
+			case 7:
+				cont7
+				
+			case 8:
+				cont8++;
+				break;
+			case 9:
+				cont9++;
+				break;
+			case 10:
+				cont10++;
+				break;
+		}
+
+	}
+	console.log("la cantidad de 1 es: "+cont1*100/cont+"%");
+	console.log("la cantidad de 2 es: "+cont2*100/cont+"%");
+	console.log("la cantidad de 3 es: "+cont3*100/cont+"%");
+	console.log("la cantidad de 4 es: "+cont4*100/cont+"%");
+	console.log("la cantidad de 5 es: "+cont5*100/cont+"%");
+	console.log("la cantidad de 6 es: "+cont6*100/cont+"%");
+	console.log("la cantidad de 7 es: "+cont7*100/cont+"%");
+	console.log("la cantidad de 8 es: "+cont8*100/cont+"%");
+	console.log("la cantidad de 9 es: "+cont9*100/cont+"%");
+	console.log("la cantidad de 10 es: "+cont10*100/cont+"%");
+	
 
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,43 @@
 function Mostrar()
 {
+	var contador=0;
+	var mascota;
+	var contadordemascota;
+	var contadordemascotasi;
+	var contadordemascotano;
+	var tipodemascota;
+	var edad;
+	var edadmayor;
 
+	for(cont=0;cont<5;cont++)
+	{
+		mascota=prompt("tiene mascota");
+		
+		while(mascota!="si" && mascota!="no")
+		{
+			mascota=prompt("tiene mascota? si o no?");
+		}
+		if(mascota=="si")
+		{
+			contadordemascotasi++;
+			tipodemascota=prompt("que tipo de mascota es? perro o gato?");
+			while(tipodemascota!="perro" && tipodemascota!="gato")
+			{
+				tipodemascota("no aceptamos esa respuesta, ¿perro o gato?")
+			}
+
+		}
+		else
+		{
+			edad=prompt("por favor ingrese su edad");
+			edadmayor=edad;
+
+			contadordemascotano++;
+
+		}
+
+	}
+
+//necesito saber el nombre del primer gato que sea de una mujer mayor a 30 años y el gato no tenga mas de cvinco años de edad
 
 }
